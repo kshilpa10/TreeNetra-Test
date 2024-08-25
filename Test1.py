73a842dbe6dc7da8423569060bdca9776e3dd2cb
Shilpa Kshirsagar <45177886+kshilpa10@users.noreply.github.com>
2024-08-25 15:21:28 +0530

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,33 @@
+# ------------Test Answers------------
+
+# 1.What is decorator , write a decorator?
+# decorative is a concept in python, also used in selenium. it is like calling a function into another function.
+# It uses '@' handler. it is called as locator
+
+# 3.WAF, S = ‘Hello everyone’, count the occurrence of each char, return those
+# repetitive character , without using any inbuilt function.'''
+# s='Hello everyone'
+# a= len(s)
+# for(i=0;i<=a;i++):
+#     if charAt(i)==s:
+#         print("the repetitive characters are: ",i)
+
+# 4.WAF , Reverse a string words.
+# > Input = ‘hello world’ > output:- ‘world hello’, without using inbuilt function
+# a='Hello World'
+# rev=a[::-1]
+# print("the reverse string is:",rev)
+
+# 10. Difference between Moudle and Packages (3 diff)
+# Module is a part of code which is executable at individual level.
+# Whereas Package is collection of different class and its subclass.
+# eg: in a calculator program- addition of 2 digits is a module and collection of similar modules in classes is called
+# print(package. package can be inbuilt or user defined also.
+
+
+# 15. Explain break, continue, pass with code?
+# break is used in switch case and loops conditional statements.
+# break immediately exits the loop when the condition is satisfied and doesnot go to next
+# iteration. whereas continue goes to skips the iteration if the condition is satisfied
+# and goes to the next iteration. continue statement is only used in loops.
+
